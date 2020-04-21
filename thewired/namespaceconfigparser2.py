486d00ba0b7bf8b3b2504ed7a8bd9f2f6f9a8998
jl <jl25019@gmail.com>
2020-04-20 21:31:13 -0400

    change default param; cleanup code

--- thewired/namespaceconfigparser2.py
+++ thewired/namespaceconfigparser2.py
@@ -5,11 +5,14 @@ from collections.abc import Mapping
 from .namespace import Namespace
 from .namespace import nsid
 
+from logging import getLogger, LoggerAdapter
+logger = getLogger(__name__)
+
 class NamespaceConfigParser2(object):
     def __init__(self, prefix: str = None):
         self.prefix = prefix if prefix else ''
 
-    def parse(self, dictConfig: dict, prefix: str = None, namespace=None, namespace_factory=Namespace):
+    def parse(self, dictConfig: dict, prefix: str = '', namespace=None, namespace_factory=Namespace):
         """
         Description:
             parse a configDict into a Namespace object
@@ -20,18 +23,21 @@ class NamespaceConfigParser2(object):
             namespace_factory - what creates the namespace object. Only tested with
                 thewired.namespace.Namespace class ATM
         """
+        class_name = self.__class__.__name__
+        log = LoggerAdapter(logger, {'name_ext' : f'{class_name}'})
+
         ns = namespace if namespace else namespace_factory()
-        rolling_prefix = prefix if prefix else ''
 
         if not dictConfig or len(dictConfig.keys()) <= 0:
             return
 
         for key in dictConfig.keys():
-            new_node_nsid = nsid.make_child_nsid(rolling_prefix, key)
-            new_node = ns.add_exactly_one(new_node_nsid)
+            new_node_nsid = nsid.make_child_nsid(prefix, key)       # give it a name
+            print(f"{new_node_nsid=}")
+            log.debug(f"{new_node_nsid=}")
+            new_node = ns.add_exactly_one(new_node_nsid)            # add one level
 
-            #- check for need to recurse
             if isinstance(dictConfig[key], Mapping):
-                self.parse(dictConfig=dictConfig[key], prefix=new_node_nsid, namespace=ns)
+               self.parse(dictConfig=dictConfig[key], prefix=new_node_nsid, namespace=ns)
 
         return ns
