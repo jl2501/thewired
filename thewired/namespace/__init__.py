4cc20c29462618d25cf7fc4e134e2dedae47581d
jl <jl25019@gmail.com>
2020-08-21 18:18:10 -0400

    Add new namespace node type
    support second lookup in another dict where the value can be determined
    at runtime via an invocation of the attribute's value object, or an
    invocation of a callable object stored in a Namespace object that is
    first looked up by treating the value as an NSID string

--- thewired/namespace/__init__.py
+++ thewired/namespace/__init__.py
@@ -1,4 +1,5 @@
 from .namespace import Namespace
 from .namespacenode import NamespaceNode
 from .namespacenode import NamespaceNodeBase
+from .namespacenode import MappedAttributesNode
 from .nsid import Nsid
