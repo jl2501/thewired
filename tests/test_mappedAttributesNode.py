aacb9e2288223b830fac1ae9a83bd7207acb5135
jl <jl25019@gmail.com>
2020-08-21 18:32:09 -0400

    add unit tests for mappedAttributesNode / SecondLifeNode

---
+++ tests/test_mappedAttributesNode.py
@@ -0,0 +1,27 @@
+import pytest
+
+from thewired.namespace import MappedAttributesNode
+
+@pytest.fixture
+def mock_attribute_map():
+    def mock_provider_callable():
+        print("mocked provider called!")
+        return "just a value to return"
+
+    return {
+        "attribute_1" : "a value for attribute_1",
+        "attribute_2" : "a.mock.provider.namespace.nsid",
+        "attribute_3" : mock_provider_callable
+    }
+
+
+
+def test_mappedAttributesInstantation(mock_attribute_map):
+    MapNode = MappedAttributesNode(nsid="test.nsid.string", attribute_map=mock_attribute_map)
+    assert str(MapNode.nsid) == 'test.nsid.string'
+    assert MapNode._attribute_map.keys() == mock_attribute_map.keys()
+
+def test_AttributeMap1(mock_attribute_map):
+    MapNode = MappedAttributesNode(nsid="test.nsid.string", attribute_map=mock_attribute_map)
+    assert MapNode.attribute_1 == mock_attribute_map.get('attribute_1')
+    assert MapNode.attribute_3 == mock_attribute_map.get('attribute_3').__call__()
