cd4f8fb18560c4deab9d596607f2a2abf24e8fef
jl <jl25019@gmail.com>
2020-08-22 00:38:25 -0400

    add back callmixin

--- thewired/namespace/namespacenode/callable.py
+++ thewired/namespace/namespacenode/callable.py
@@ -3,25 +3,12 @@ python dunder / magic methods can't be added dynamically
 but I don't want to include things that aren't used as it clutters
 the logical namespace navigation xp.
 
-a CallableNode is a namespace node that defines __call__ and thus can be invoked / called
+a CallableNodeMixin is a namespace node Mixin that defines __call__ and thus can be invoked / called
 """
 
-from .base import NamespaceNodeBase
-
-class CallableNode(NamespaceNodeBase):
+class CallableNodeMixin(object):
     """
     A Namespace Node with a __call__ method registered in the native slots
     """
-    def __init__(self, nsid, __call__=None):
-        """
-        Description:
-            create a callable object that calls the passed in callable, __call__, when called. %-D
-        Input:
-            nsid: namespace id
-            __call__: the callable to invoke in response to us being invoked
-        """
-        super().__init__(nsid)
-        self.target_callable = __call__
-        
     def __call__(self, *args, **kwargs):
-        return self.target_callable(*args, **kwargs)
+        return self.invoke(*args, **kwargs)
