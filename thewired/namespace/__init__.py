d22578284e61b74551fe83bb7ab7b42e72ab0f37
jl <jl25019@gmail.com>
2020-08-21 21:30:21 -0400

    add callablenode to namespace api

--- thewired/namespace/__init__.py
+++ thewired/namespace/__init__.py
@@ -2,4 +2,5 @@ from .namespace import Namespace
 from .namespacenode import NamespaceNode
 from .namespacenode import NamespaceNodeBase
 from .namespacenode import MappedAttributesNode
+from .namespacenode import CallableNode
 from .nsid import Nsid
