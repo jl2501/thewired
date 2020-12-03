cde54cdeea49f7c89d9f28cb48da1b6ecd653ef4
jl <jl25019@gmail.com>
2020-12-02 21:55:47 -0500

    add ability to override node factory in config
    still needs to be subclass of NamespaceNodeBase else all bets are off

--- tests/test_configparser.py
+++ tests/test_configparser.py
@@ -4,6 +4,7 @@ import unittest
 from thewired.namespace import Namespace
 from thewired.namespace import NamespaceNode
 from thewired.namespace import NamespaceNodeBase
+from thewired.namespace.nsid import get_nsid_ancestry
 from thewired import NamespaceConfigParser
 from thewired import NamespaceConfigParser2
 
@@ -29,7 +30,7 @@ class test_NamespaceConfigParser1(unittest.TestCase):
 
 
 
-class test_NamespaceConfigParser2():
+class test_NamespaceConfigParser2(unittest.TestCase):
     def test_NamespaceConfigParser_instantation(self):
         nscp = NamespaceConfigParser2()
 
@@ -37,16 +38,54 @@ class test_NamespaceConfigParser2():
         test_dict = {
             "all" : {
                 "work" : {
-                    "no_play" : "dull_boy"
+                    "no" : {
+                      "play" : {
+                          "dull_boy" : {}
+                      }
+                    }
                 }
             },
             "hackers" : {
                 "on" : {
-                    "planet" : "earth"
+                    "planet" : {
+                        "earth" : {}
+                    }
                 }
             }
         }
 
         nscp = NamespaceConfigParser2(prefix='')
         ns = nscp.parse(dictConfig=test_dict)
-        assert isinstance(ns.hackers, NamespaceNodeBase)
+        for nsid in get_nsid_ancestry('.all.work.no.play.dull_boy'):
+            assert isinstance(ns.get(nsid), NamespaceNodeBase)
+
+
+        for nsid in get_nsid_ancestry('.hackers.on.planet.earth'):
+            assert isinstance(ns.get(nsid), NamespaceNodeBase)
+
+
+    def test_parse_meta(self):
+        test_dict = {
+            "topkey" : {
+                "user1" : {
+                    "__type__": "thewired.SecondLifeNode",
+                    "__init__": {
+                        "a": "a value for param1",
+                        "b" : "param2's value"
+                    }
+                }
+            }
+        }
+
+        nscp = NamespaceConfigParser2()
+        ns = nscp.parse(dictConfig=test_dict)
+        print(ns.walk())
+
+        assert isinstance(ns.root, NamespaceNodeBase)
+
+        from thewired import SecondLifeNode
+        assert isinstance(ns.get(".topkey.user1"), SecondLifeNode)
+        assert ns.get('.topkey.user1').a == "a value for param1"
+        assert ns.get('.topkey.user1').b == "param2's value"
+        assert ns.root.topkey.user1.a == "a value for param1"
+        assert ns.root.topkey.user1.b == "param2's value"
--- thewired/namespaceconfigparser2.py
+++ thewired/namespaceconfigparser2.py
@@ -1,18 +1,85 @@
+import sys
 import typing
-from typing import Dict
+from typing import Dict, Union
 from collections.abc import Mapping
+from importlib import import_module
+from functools import partial
 
 from .namespace import Namespace
+from .namespace import NamespaceNodeBase
 from .namespace import nsid
 
 from logging import getLogger, LoggerAdapter
 logger = getLogger(__name__)
 
 class NamespaceConfigParser2(object):
-    def __init__(self, prefix: str = None):
+    def __init__(self, prefix: str = None, node_factory=NamespaceNodeBase):
         self.prefix = prefix if prefix else ''
+        self.default_node_factory=node_factory
 
-    def parse(self, dictConfig: dict, prefix: str = '', namespace=None, namespace_factory=Namespace):
+        #- special YAML keys that can be used to let this parser know
+        #- what type should be used for the node factory and what params to pass it
+        self.meta_keys = ['__type__', '__init__']
+
+
+            
+
+    def _get_node_factory(self, key: str, dictConfig: dict) -> partial:
+        """
+        Description:
+            return a context manager that sets the node factory based on any meta keys that
+            may exist under this key
+        """
+        log = LoggerAdapter(logger, dict(name_ext=f'{self.__class__.__name__}._parse_meta_key'))
+
+        #- check the subkeys of this key to see if they have any meta info about how to create the node
+        if any(x in self.meta_keys for x in dictConfig[key].keys()):
+            #- we need to change the node factory from the default
+            try:
+                nf_module_name = '.'.join(dictConfig[key]["__type__"].split('.')[0:-1])
+                nf_symbol_name = dictConfig[key]["__type__"].split('.')[-1]
+                nf_module = import_module(nf_module_name)
+
+            except KeyError:
+                #- no "__type__" key
+                #- leave node_factory set to the default
+                log.debug("key error when trying to access '__type__'")
+                nf_module = None
+
+            except ValueError:
+                #- we have a name, but it might not have a dot at all,
+                #- which would then try to import the empty string and 
+                #- fail with a ValueError
+
+                #- try to use the current module as the module containing the node factory
+                log.debug("value error importing namespace factory module: \"{nf_module_name}\"")
+                nf_module = sys.modules[__name__]
+
+            finally:
+                if nf_module:
+                    node_factory = getattr(nf_module, nf_symbol_name)
+                else:
+                    node_factory = self.default_node_factory
+
+            #- set the parameters for the node factory
+            try:
+                _init_params = dictConfig[key]["__init__"]
+            except KeyError:
+                #- no __init__ key
+                _init_params = dict()
+
+
+            log.debug(f"returning custom {node_factory=} {_init_params=}")
+            return partial(node_factory, **_init_params)
+
+        else:
+            log.debug("returning default node factory")
+            return self.default_node_factory
+            
+
+                
+
+    def parse(self, dictConfig: dict, prefix:str='', namespace:Union[None, Namespace]=None, namespace_factory:type=Namespace) -> Union[Namespace, None]:
         """
         Description:
             parse a configDict into a Namespace object
@@ -29,17 +96,29 @@ class NamespaceConfigParser2(object):
         """
         log = LoggerAdapter(logger, dict(name_ext=f'{self.__class__.__name__}.parse'))
 
+        log.debug(f"enter: {prefix=} {namespace=} {namespace_factory=} {dictConfig=}")
         ns = namespace if namespace else namespace_factory()
 
         if not dictConfig or len(dictConfig.keys()) <= 0:
-            return
+            return None
 
+        #- create a namespace of objects described by dictConfig Mapping
         for key in dictConfig.keys():
-            new_node_nsid = nsid.make_child_nsid(prefix, key)
-            log.debug(f"{new_node_nsid=}")
-            new_node = ns.add_exactly_one(new_node_nsid)
+            log.debug(f"starting {key=}")
+            if key not in self.meta_keys:
+                node_factory = self._get_node_factory(key, dictConfig)
 
-            if isinstance(dictConfig[key], Mapping):
-               self.parse(dictConfig=dictConfig[key], prefix=new_node_nsid, namespace=ns)
+                #- every key gets turned into a node in the namespace
+                new_node_nsid = nsid.make_child_nsid(prefix, key)
+                log.debug(f"{new_node_nsid=}")
+                log.debug(f"{node_factory=}")
 
+                new_node = ns.add_exactly_one(new_node_nsid, node_factory)
+
+                if isinstance(dictConfig[key], Mapping):
+                   log.debug(f"recursing on {key=} {dictConfig[key]}")
+                   self.parse(dictConfig=dictConfig[key], prefix=new_node_nsid, namespace=ns)
+                else:
+                    log.debug(f"setting {new_node.nsid}.{key} to {dictConfig[key]}")
+                    setattr(new_node, key, dictConfig[key])
         return ns
